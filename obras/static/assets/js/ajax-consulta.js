4920dde437f3a8c4d117133734c1246d7cb6a90c
Alex_Neutron <ags_neutron@hotmail.com>
2015-05-06 09:36:45 -0500

    consulta general 2

--- obras/static/assets/js/ajax-consulta.js
+++ obras/static/assets/js/ajax-consulta.js
@@ -4,42 +4,44 @@
 /**
  * Created by db2 on 7/04/15.
  */
-$(document).on('ready', main_consulta);
+var $j = jQuery.noConflict();
+$j(document).on('ready', main_consulta);
 
 function main_consulta() {
-    $.ajaxSetup({
+    $j.ajaxSetup({
 		beforeSend: function(xhr, settings) {
 			if(settings.type == "POST"){
-				xhr.setRequestHeader("X-CSRFToken", $('[name="csrfmiddlewaretoken"]').val());
+				xhr.setRequestHeader("X-CSRFToken", $j('[name="csrfmiddlewaretoken"]').val());
 			}
             if(settings.type == "GET"){
-				xhr.setRequestHeader("X-CSRFToken", $('[name="csrfmiddlewaretoken"]').val());
+				xhr.setRequestHeader("X-CSRFToken", $j('[name="csrfmiddlewaretoken"]').val());
 			}
 		}
 	});
 
-	$('#ver_datos button').on('click', verDatos);
+	$j('#ver_datos button').on('click', verDatos);
 }
 
 
 
 
 function verDatos() {
-    var arrayDependencias = $("#msDependencias").multiselect("getChecked").map(function(){
+    var arrayDependencias = $j("#msDependencias").multiselect("getChecked").map(function(){
                                          return this.value;
                                     }).get();
 
     alert(arrayDependencias);
-    $.ajax({
+    $j.ajax({
         url: '/obras/api/busqueda',
         type: 'get',
         data: {
-            access_token: 'KRmZyQFQJAUPUQ0tOyHhwfkQ56opZ3',
+            access_token: 'KSGhWbjEGiBbePxl0EuGup8rreu4Vk',
             dependencia:arrayDependencias.toString()
         },
         success: function(data) {
             //$('#datos').html
             alert(data.obras[0].tipoObra.nombreTipoObra);
+            tabla(data);
 
         },
         error: function(data) {
@@ -47,4 +49,65 @@ function verDatos() {
         }
     });
     //$.get('http://127.0.0.1:8000/obras/consultar-obras');
+}
+
+function tabla(Datos){
+    var sHtml='<table cellspacing="1" class="tablesorter">'
+                    +'<thead>'
+                        +'<tr>'
+                            +'<th>ID</th>'
+                            +'<th>DENOMINACI&Oacute;N</th>'
+                            +'<th>ESTADO</th>'
+                        +'</tr>'
+                    +'</thead>'
+                    +'<tfoot>'
+                        +'<tr>'
+                            +'<th>ID</th>'
+                            +'<th>DENOMINACI&Oacute;N</th>'
+                            +'<th>ESTADO</th>'
+                        +'</tr>'
+
+                        +'<tr><td class="pager" id="pager" colspan="3"><form>'
+                        +'<img src="../../static/assets/tablesorter/addons/pager/icons/first.png" class="first"/>'
+                        +'<img src="../../static/assets/tablesorter/addons/pager/icons/prev.png" class="prev"/>'
+                        +'<input type="text" class="pagedisplay"/>'
+                        +'<img src="../../static/assets/tablesorter/addons/pager/icons/next.png" class="next"/>'
+                        +'<img src="../../static/assets/tablesorter/addons/pager/icons/last.png" class="last"/>'
+                        +'<select class="pagesize">'
+                        +'<option selected="selected"  value="10">10</option>'
+                        +'    <option value="20">20</option>'
+                        +'    <option value="30">30</option>'
+                        +'    <option  value="40">40</option>'
+                        +'</select></form></td></tr>'
+
+                    +'</tfoot>'
+                    +'<tbody>';
+    for(var i= 0;i<Datos.obras.length;i++){
+        sHtml +='<tr>'
+                +'<td>' + i +'</td>'
+                +'<td>' + Datos.obras[i].denominacion +'</td>'
+                +'<td>' + Datos.obras[i].estado.nombreEstado +'</td>'
+                +'</tr>'
+    }
+
+        sHtml +='</tbody>'
+                +'</table>'
+                +'<script type="text/javascript">'
+                +'$(function() {'
+                +'    $("table")'
+                +'        .tablesorter({widthFixed: true,showProcessing: true,headerTemplate : "{content} {icon}", widgets: ["zebra", "scroller"],'
+                +'          widgetOptions : {'
+                +'          scroller_height : 300,'
+                +'          scroller_upAfterSort: true,'
+                +'          scroller_jumpToHeader: true,'
+                +'          scroller_barWidth : null}'
+                +'        })'
+                +'        .tablesorterPager({container: $("#pager")});'
+                +'});'
+                +'</script>';
+
+
+
+
+    $j('#datos').html(sHtml);
 }
