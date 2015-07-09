2d8d496ced3716f18ec010bcdc20f66695eca526
Pedro Contreras <fpedro23@gmail.com>
2015-07-09 13:24:02 -0500

    Ipad Serialization

--- obras/urls.py
+++ obras/urls.py
@@ -24,7 +24,7 @@ urlpatterns = patterns('',
                        url(r'^api/subdependencias_flat', api.SubependenciasFlatEndpoint.as_view()),
                        url(r'^api/dependencia_imagen', api.DependenciasIdEndpoint.as_view()),
                        url(r'^api/idUnico', api.IdUnicoEndpoint.as_view()),
-                       url(r'^api/idUnicoIpad', api.IdUnicoEndpointIpad.as_view()),
+                       url(r'^api/idIpad', api.IdUnicoEndpointIpad.as_view()),
 
 
 
