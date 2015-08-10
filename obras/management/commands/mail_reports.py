a8aec600609c46d610e7587e108cf0d949269a88
Marcelo Gomez <marcelogomez321@gmail.com>
2015-08-10 16:16:42 -0500

    Bug fix

--- obras/management/commands/mail_reports.py
+++ obras/management/commands/mail_reports.py
@@ -9,24 +9,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         today = datetime.now()
 
-        print '[%s/%s/%s %s:%s:%s] Inicializando envio de correos' % today.day, today.month, today.year, today.hour, today.minute, today.second
+        print '[{0}/{1}/{2} {3}:{4}:{5}] Inicializando envio de correos'.format(today.day, today.month, today.year, today.hour, today.minute, today.second)
 
         for dependencia in Dependencia.objects.all():
-            print '[%s/%s/%s %s:%s:%s] Buscando obras para dependencia %s' % today.day, today.month, today.year, today.hour, today.minute, today.second, dependencia.nombreDependencia
+            print '[{0}/{1}/{2} {3}:{4}:{5}] Buscando obras para dependencia {6}'.format(today.day, today.month, today.year, today.hour, today.minute, today.second, dependencia.nombreDependencia)
             obras = Obra.objects.filter(Q(dependencia=dependencia) & Q(autorizada=False))
 
-            content = 'Las siguientes obras de la dependencia %s tienen cambios sin autorizar: \n' % dependencia.nombreDependencia
+            content = 'Las siguientes obras de la dependencia {0} tienen cambios sin autorizar: \n'.format(dependencia.nombreDependencia)
             for obra in obras:
-                content += '\t- %s\n' % obra.identificador_unico
+                content += '\t- {0}\n'.format(obra.identificador_unico)
 
-            print '[%s/%s/%s %s:%s:%s] Enviando correos' % today.day, today.month, today.year, today.hour, today.minute, today.second
+            print '[{0}/{1}/{2} {3}:{4}:{5}] Enviando correos'.format(today.day, today.month, today.year, today.hour, today.minute, today.second)
             for contacto in dependencia.get_contactos():
                 try:
                     send_mail('Reporte de obras por autorizar',
                           content,
                           'edicomexsa@gmail.com', [contacto.email])
-                    print '[%s/%s/%s %s:%s:%s] Correo enviado a %s' % today.day, today.month, today.year, today.hour, today.minute, today.second, contacto.user.email
+                    print '[{0}/{1}/{2} {3}:{4}:{5}] Correo enviado a {6}'.format(today.day, today.month, today.year, today.hour, today.minute, today.second, contacto.user.email)
                 except Exception as e:
-                    print '[%s/%s/%s %s:%s:%s] Error al enviar correo a %s' % today.day, today.month, today.year, today.hour, today.minute, today.second, contacto.user.email
-                    print '[%s/%s/%s %s:%s:%s] %s' % today.day, today.month, today.year, today.hour, today.minute, today.second, e.message
+                    print '[{0}/{1}/{2} {3}:{4}:{5}] Error al enviar correo a {6}'.format(today.day, today.month, today.year, today.hour, today.minute, today.second, contacto.user.email)
+                    print '[{0}/{1}/{2} {3}:{4}:{5}] {6}'.format(today.day, today.month, today.year, today.hour, today.minute, today.second, e.message)
             print 'Finalizando envio de correos'
