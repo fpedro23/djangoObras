893f88fc1b8aa490681f4bba6febf17afaad33e6
Marcelo Gomez <marcelogomez321@gmail.com>
2015-09-01 20:42:02 -0500

    Filtrado de municipios en el form de actualizar obra

--- obras/static/assets/js/ajax-estados-municipios.js
+++ obras/static/assets/js/ajax-estados-municipios.js
@@ -4,8 +4,35 @@
 var $j = jQuery.noConflict();
 
 $j(document).on('ready', function() {
-    if ($('#id_municipio').find('option:selected').val() == "")
+    if ($('#id_municipio').find('option:selected').val() == "") {
         clearMunicipios();
+    }
+    else {
+        var municipioId = $('#id_municipio').find('option:selected').val()
+
+        $j.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if(settings.type == "POST")
+                    xhr.setRequestHeader("X-CSRFToken", $j('[name="csrfmiddlewaretoken"]').val());
+                if(settings.type == "GET")
+                    xhr.setRequestHeader("X-CSRFToken", $j('[name="csrfmiddlewaretoken"]').val());
+            }
+        });
+
+        $.get('/obras/register-by-token', function(ans) {
+            var ajaxData = { access_token: ans.access_token, estados: estadoId };
+
+            $j.ajax({
+                url: '/obras/api/municipios_por_estado',
+                type: 'get',
+                data: ajaxData,
+                success: function(ans) {
+                    populateMunicpiosSelect(ans);
+                    $('#id_municipio').select(municipioId);
+                }
+            });
+        });
+    }
     $('#id_estado').on('change', function() {
         var option = $(this).find('option:selected');
 
