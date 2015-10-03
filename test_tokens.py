b522f3baccb2762920da1ab44e7b48f94b74bc0b
Marcelo Gomez <marcelogomez321@gmail.com>
2015-10-03 12:06:31 -0500

    Merge obras_history

--- test_tokens.py
+++ test_tokens.py
@@ -1,12 +1,11 @@
 import requests
-import django.db.backends.mysql.base
 
-SECRET = 'Ito4EuoY5hhWuOynPlOxJkgiAguO2tHpFsIZHgkh6HMjN4gHLsOdbOmHxMitMAdJs9FoJS5lK9DiVIiurS6CeocgCVgKHR6JEBt69HY1nsWXaE1bFA7ZpjNQLbrhlgpf'
+SECRET = '5K7TIczTuazzwEY5t6qmWItNoqFQNimzgGQbOpR1Y9q9Pe9iamP4gnT6FI7TUlfuZoSEhZ4BdGSngwb7tqOhHmwv497ExPUSsI2BT8vRhcXWD5FLzPgHvMHh7AXVJI6y'
 payload = {
     'grant_type': 'password',
     'username': 'capusu',
     'password': 'capusu',
-    'client_id': 'nJKmO290WGy5TY4GAmxK23cg8mgTDaYf9bgfE5Mx',
+    'client_id': '2GNk4k1QcEPlMmXidG2nFshqvn1Eu8Y0P6t26DC6',
     'client_secret': SECRET,
     'scope': 'write read'
 }
